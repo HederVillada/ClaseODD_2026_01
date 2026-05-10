b61411e3846cc421493e01d0361192f01fe96ac7
HederVillada <heder.villada@campusucc.edu.co>
2026-05-10 14:05:31 +0000

    Cambiado el main para ser mucho mas amplio y mejor formulado en gameplay, cambiado MagDump para verse mas como trickshot

--- Juego/Abilities/MagDump.java
+++ Juego/Abilities/MagDump.java
@@ -6,26 +6,43 @@ import java.util.Random;
 public class MagDump implements Ability {
     @Override
     public String getName() { return "Mag Dump"; }
+
     @Override
     public boolean isPassive() { return false; }
+
     @Override
-    public double getAccuracyMult() { return 0.33; }
+    public double getAccuracyMult() { return 0.33; } // Significant accuracy penalty
 
     @Override
     public void execute(Personaje user, Personaje target) {
-        int shots = user.activeWeapon.currentAmmo;
-        if (shots <= 0) return;
+        int shotsToFire = user.activeWeapon.currentAmmo;
+        
+        if (shotsToFire <= 0) {
+            System.out.println("! Click... The magazine is bone dry.");
+            return;
+        }
 
-        // FIX: Use getNombre()
-        System.out.println(">> " + user.getNombre() + " empties the magazine!");
+        System.out.println(">> " + user.getNombre() + " SCREAMS AND DRAINS THE MAG!");
         Random rand = new Random();
-        double finalAcc = user.accuracy * user.activeWeapon.type.accMultiplier * 0.33;
+        
+        // Calculation using the weapon type multiplier and the 0.33 ability penalty
+        double finalAcc = user.accuracy * user.activeWeapon.getType().accMultiplier * 0.33;
 
-        for (int i = 0; i < shots; i++) {
-            user.activeWeapon.currentAmmo--;
+        for (int i = 1; i <= shotsToFire; i++) {
+            user.activeWeapon.currentAmmo--; 
+            
             if (rand.nextInt(100) < finalAcc) {
-                target.takeDamage(user.activeWeapon.type.minDmg);
+                int min = user.activeWeapon.getType().minDmg;
+                int max = user.activeWeapon.getType().maxDmg;
+                int dmg = rand.nextInt((max - min) + 1) + min;
+                
+                target.takeDamage(dmg);
+                System.out.println("   [!] Round #" + i + " CONNECTED! Dealt " + dmg + " damage.");
+            } else {
+                System.out.println("   [ ] Round #" + i + " missed wide.");
             }
         }
+        
+        System.out.println(">> LOCK TO REAR. " + user.getNombre() + " is out of ammo!");
     }
 }
--- Juego/JuegoTactico.java
+++ Juego/JuegoTactico.java
@@ -7,7 +7,7 @@ import java.util.Random;
 public class JuegoTactico {
     private Personaje jugador1;
     private Personaje jugador2;
-    private Scanner sc = new Scanner(System.in);
+    private static Scanner sc = new Scanner(System.in);
     private Random rand = new Random();
 
     public JuegoTactico (Personaje p1, Personaje p2) {
@@ -15,28 +15,30 @@ public class JuegoTactico {
         this.jugador2 = p2;
     }
 
-    //aqui esta el metodo que inicia la pelea, se encarga de ejecutar el loop del juego, y de determinar el ganador al final de la pelea
+    // aqui esta el metodo que inicia la pelea, se encarga de ejecutar el loop del juego, y de determinar el ganador al final de la pelea
     public void iniciarPelea() {
         System.out.println("\nENGAGEMENT INITIATED: " + jugador1.getNombre() + " VS " + jugador2.getNombre());
 
-        //loop basico del juego, se ejecuta mientras ambos personajes esten vivos, y se alternan los turnos entre el jugador y la IA
+        // loop basico del juego, se ejecuta mientras ambos personajes esten vivos, y se alternan los turnos entre el jugador y la IA
         while (jugador1.estaVivo() && jugador2.estaVivo()) {
-            ejecutarTurno(jugador1, jugador2, false); //turno jugador
+            ejecutarTurno(jugador1, jugador2, false); // turno jugador
             if (!jugador2.estaVivo()) break;
             
-            ejecutarTurno(jugador2, jugador1, true);  //turno IA
+            ejecutarTurno(jugador2, jugador1, true);  // turno IA
         }
 
-        //determina el ganador al final de la pelea, dependiendo de quien este vivo, o muerto, respectivamente
+        // determina el ganador al final de la pelea, dependiendo de quien este vivo, o muerto, respectivamente
         System.out.println("\n" + (jugador1.estaVivo() ? ">>> " + jugador1.getNombre() + " SECURED THE SECTOR." : ">>> " + jugador1.getNombre() + " IS KIA."));
     }
 
     private void ejecutarTurno(Personaje p, Personaje target, boolean isAI) {
-        System.out.println("\n--- " + p.getNombre() + " | HP: " + p.getPuntosDeVida() + " | ACC: " + p.accuracy + "% ---");
+        //el UI para cada turno
+        System.out.println("\n====================================================");
+        System.out.println("--- " + p.getNombre() + " | HP: " + p.getPuntosDeVida() + " | ACC: " + p.accuracy + "% ---");
         System.out.println("WEAPON: " + p.activeWeapon.getName() + " [" + p.activeWeapon.currentAmmo + "/" + p.activeWeapon.maxAmmo + "]");
         
         if (p.isBusy()) {
-            System.out.println(">> " + p.currentAction + " (" + p.turnsToWait + " turns left)");
+            System.out.println(">> " + p.getNombre() + " is busy: " + p.currentAction + " (" + p.turnsToWait + " turns left)");
             p.turnsToWait--;
             return;
         }
@@ -46,63 +48,154 @@ public class JuegoTactico {
         } else {
             handleOpponentAI(p, target);
         }
+        
+        // POST-ACTION STATUS UPDATE
+        System.out.println("STATUS: " + target.getNombre() + " HP is now " + target.getPuntosDeVida());
+        System.out.println("====================================================");
     }
 
     private void handlePlayerInput(Personaje p, Personaje target) {
-        System.out.println("1. SHOOT | 2. SWAP | 3. RELOAD | 4. " + p.activeWeapon.getAbility1().getName() + " | 5. " + p.activeWeapon.getAbility2().getName()); //las abilidades cambian dependiendo el arma
+        System.out.println("1. SHOOT | 2. SWAP | 3. RELOAD | 4. " + p.activeWeapon.getAbility1().getName() + " | 5. " + p.activeWeapon.getAbility2().getName()); 
         String move = sc.nextLine();
         switch (move) {
-            case "1": p.shoot(target); break;
-            case "2": p.swapWeapon(); break;
-            case "3": p.startReload(); break;
-            case "4": p.activeWeapon.getAbility1().execute(p, target); break;
-            case "5": p.activeWeapon.getAbility2().execute(p, target); break;
-            default: System.out.println(">> Hesitation. Turn skipped.");
+            case "1": 
+                System.out.println(">> " + p.getNombre() + " pulls the trigger!");
+                p.shoot(target); 
+                break;
+            case "2": 
+                p.swapWeapon(); 
+                break;
+            case "3": 
+                p.startReload(); 
+                break;
+            case "4": 
+                System.out.println(">> " + p.getNombre() + " ACTIVATES: " + p.activeWeapon.getAbility1().getName());
+                p.activeWeapon.getAbility1().execute(p, target); 
+                break;
+            case "5": 
+                System.out.println(">> " + p.getNombre() + " ACTIVATES: " + p.activeWeapon.getAbility2().getName());
+                p.activeWeapon.getAbility2().execute(p, target); 
+                break;
+            default: 
+                System.out.println(">> [!] INVALID INPUT: " + move + ". YOU FROZE IN COMBAT!");
         }
     }
 
     private void handleOpponentAI(Personaje p, Personaje target) {
-        //DECISIONES DE LA IA
         int decision = rand.nextInt(100);
 
-        //Recarga si esta vacio
         if (p.activeWeapon.currentAmmo <= 0) {
-            System.out.println(">> " + p.getNombre() + " is dry! Reloading...");
+            System.out.println(">> " + p.getNombre() + " SELECTED: RELOAD");
             p.startReload();
         } 
-        //Puede usar abilidad 1 o 2
         else if (decision < 20) {
-            System.out.println(">> " + p.getNombre() + " uses " + p.activeWeapon.getAbility1().getName() + "!");
+            System.out.println(">> " + p.getNombre() + " SELECTED: " + p.activeWeapon.getAbility1().getName());
             p.activeWeapon.getAbility1().execute(p, target);
         } 
         else if (decision < 40) {
-            System.out.println(">> " + p.getNombre() + " uses " + p.activeWeapon.getAbility2().getName() + "!");
+            System.out.println(">> " + p.getNombre() + " SELECTED: " + p.activeWeapon.getAbility2().getName());
             p.activeWeapon.getAbility2().execute(p, target);
         }
-        //Puede cambiar armas
         else if (decision < 50) {
+            System.out.println(">> " + p.getNombre() + " SELECTED: SWAP WEAPON");
             p.swapWeapon();
-            System.out.println(">> " + p.getNombre() + " is switching to " + p.activeWeapon.getName() + "!");
         }
-        //Y si no, tan solo dispara. Bonito my uso de ifs, elifs, y elses, no?
         else {
+            System.out.println(">> " + p.getNombre() + " SELECTED: SHOOT");
             p.shoot(target);
         }
     }
 
+    private static Weapon selectWeaponMenu() {
+        while (true) {
+            System.out.println("\n--- CATEGORIES ---");
+            System.out.println("1. BATTLE RIFLES | 2. ASSAULT RIFLES | 3. SHOTGUNS | 4. SIDEARMS");
+            System.out.print("SELECT: ");
+            String cat = sc.nextLine();
+            
+            if (cat.equals("1")) {
+                while (true) {
+                    System.out.println("\n[BR] 1. G3 | 2. FAL 50.00 | 3. Browning BLR | 0. BACK");
+                    String c = sc.nextLine();
+                    if (c.equals("1")) return new G3();
+                    if (c.equals("2")) return new FAL50();
+                    if (c.equals("3")) return new BrowningBLR();
+                    if (c.equals("0")) break;
+                    System.out.println(">> [!] ERROR: INVALID SELECTION. TRY AGAIN.");
+                }
+            } else if (cat.equals("2")) {
+                while (true) {
+                    System.out.println("\n[AR] 1. AKM | 2. AK-74M | 3. FAL 7.62 (Soviet) | 0. BACK");
+                    String c = sc.nextLine();
+                    if (c.equals("1")) return new AKM();
+                    if (c.equals("2")) return new AK74M();
+                    if (c.equals("3")) return new FAL762();
+                    if (c.equals("0")) break;
+                    System.out.println(">> [!] ERROR: INVALID SELECTION. TRY AGAIN.");
+                }
+            } else if (cat.equals("3")) {
+                while (true) {
+                    System.out.println("\n[SG] 1. Auto-5 | 2. OverUnder | 0. BACK");
+                    String c = sc.nextLine();
+                    if (c.equals("1")) return new Auto5();
+                    if (c.equals("2")) return new OverUnder();
+                    if (c.equals("0")) break;
+                    System.out.println(">> [!] ERROR: INVALID SELECTION. TRY AGAIN.");
+                }
+            } else if (cat.equals("4")) {
+                while (true) {
+                    System.out.println("\n[SIDE] 1. MAC-10 | 2. M60 | 3. Bodyguard 49 | 4. Makarov | 0. BACK");
+                    String c = sc.nextLine();
+                    if (c.equals("1")) return new MAC10();
+                    if (c.equals("2")) return new M60();
+                    if (c.equals("3")) return new Bodyguard49();
+                    if (c.equals("4")) return new Makarov();
+                    if (c.equals("0")) break;
+                    System.out.println(">> [!] ERROR: INVALID SELECTION. TRY AGAIN.");
+                }
+            } else {
+                System.out.println(">> [!] ERROR: CATEGORY '" + cat + "' DOES NOT EXIST.");
+            }
+        }
+    }
+
+    private static Weapon getRandomWeapon() {
+        Random r = new Random();
+        Weapon[] pool = {new AKM(), new G3(), new M60(), new Auto5(), new MAC10(), new Makarov()};
+        return pool[r.nextInt(pool.length)];
+    }
+
     public static void main(String[] args) {
-        Scanner input = new Scanner(System.in);
         System.out.println("★ TACTICAL SIMULATOR 2026 ★");
         
         System.out.print("ENTER OPERATOR NAME: ");
-        String p1Name = input.nextLine();
-        
-        System.out.print("ENTER OPPONENT NAME: ");
-        String p2Name = input.nextLine(); //escojes el nombre del oponente, pero no es necesario, ya que el oponente es controlado por la IA
+        String p1Name = sc.nextLine();
+        System.out.println("\n--- PLAYER 1: SELECT PRIMARY ---");
+        Weapon p1P = selectWeaponMenu();
+        System.out.println("\n--- PLAYER 1: SELECT SECONDARY ---");
+        Weapon p1S = selectWeaponMenu();
 
-        //aqui es donde se crean los personajes, con sus armas primarias y secundarias, respectivamente
-        Personaje p1 = new Personaje(p1Name, new G3(), new Makarov());
-        Personaje p2 = new Personaje(p2Name, new AKM(), new Bodyguard49());
+        System.out.print("\nENTER OPPONENT NAME: ");
+        String p2Name = sc.nextLine(); 
+        
+        System.out.println("\n--- CONFIGURE OPPONENT LOADOUT ---");
+        System.out.println("1. MANUAL SELECTION | 2. RANDOMIZER");
+        String setup = sc.nextLine();
+        
+        Weapon p2P, p2S;
+        if (setup.equals("1")) {
+            System.out.println("\n--- OPPONENT: SELECT PRIMARY ---");
+            p2P = selectWeaponMenu();
+            System.out.println("\n--- OPPONENT: SELECT SECONDARY ---");
+            p2S = selectWeaponMenu();
+        } else {
+            p2P = getRandomWeapon();
+            p2S = getRandomWeapon();
+            System.out.println(">> Rerolling weapons for " + p2Name + "...");
+        }
+
+        Personaje p1 = new Personaje(p1Name, p1P, p1S);
+        Personaje p2 = new Personaje(p2Name, p2P, p2S);
 
         JuegoTactico juego = new JuegoTactico(p1, p2);
         juego.iniciarPelea();
